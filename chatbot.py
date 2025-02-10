afe0d40ae49713f3b64f52d1a0ca3db55179a1f0
VIJAYABALAN <vijayabalan30042001@gmail.com>
2025-02-10 09:58:19 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -30,7 +30,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="VIJAY@25",
+        password="VXCXXX5",
         database="SmartInventory"
     )
 
